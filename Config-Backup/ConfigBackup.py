21aea17d2545762dcd876e03a95936804093d095
root <root@SPS-ScriptEngine>
2017-11-15 09:15:03 -0600

    Adding Config Backup Script

---
+++ Config-Backup/ConfigBackup.py
@@ -0,0 +1,36 @@
+
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from getpass import getpass
+import datetime as dt
+import os
+
+username = 'root'
+password = getpass("Root Password:")
+
+
+#Open file named switchlist-Complete 
+with open('switchlist-Complete') as infile:
+        for host in infile:
+                try:
+
+		    
+		    dev = Device(host=host.strip(), user=username, password=password)
+		    print 'Working on:' , host
+		    dev.open()
+		    date = dt.datetime.today().strftime("%m-%d-%Y")
+		    config = dev.rpc.get_config(options={'format':'set'})
+                    #Destination location for Config backups
+		    ConfigFile = '/' + date + "/" + host + '.txt'
+		    a = os.path.dirname(ConfigFile)
+		    if not os.path.exists(a):
+		        os.makedirs(a)
+		    from lxml import etree
+
+		    f = open(ConfigFile, 'w')
+		    f.write(etree.tostring(config))
+		    f.close()
+		    dev.close()
+		except Exception,e: print "Error:", e
+		continue
+
